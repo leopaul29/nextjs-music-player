99d99f33efbe6ec705f53127d5d26eb547223f14
Leo-paul <leopaulmartin@hotmail.fr>
2021-02-07 02:55:23 +0100

    add song list api

---
+++ pages/api/songs.js
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req, res) => {
+  res.statusCode = 200;
+  res.json([
+    {
+      index: 0,
+      path: "musics/Pandrezz - Mithril (Beat Tape) - 01 Drippin'.mp3",
+      title: "Drippin",
+      duration: "2:49",
+    },
+    {
+      index: 1,
+      path: "musics/Pandrezz - Mithril (Beat Tape) - 02 dating you.mp3",
+      title: "Dating you",
+      duration: "2:02",
+    },
+    {
+      index: 2,
+      path: "musics/Pandrezz - Mithril (Beat Tape) - 03 leaving you.mp3",
+      title: "Leaving you",
+      duration: "2:10",
+    },
+    {
+      index: 3,
+      path:
+        "musics/Pandrezz - Mithril (Beat Tape) - 04 san francisco (feat. Idealism).mp3",
+      title: "San Francisco",
+      duration: "2:30",
+    },
+    {
+      index: 4,
+      path:
+        "musics/Pandrezz - Mithril (Beat Tape) - 05 I'm Sorry For This Love Ballad.mp3",
+      title: "I'm Sorry For This Love Ballad",
+      duration: "2:27",
+    },
+  ]);
+};
